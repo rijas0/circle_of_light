16bee0c2f3e9cad18dcc19f9db83194d00303d25
althaf <althafahamed47@gmail.com>
2026-05-01 15:08:09 +0000

    Add OAuth dependencies for Quran Foundation integration
    - Add flutter_appauth ^8.0.0 for OIDC flow
    - Add flutter_secure_storage ^9.2.2 for token persistence

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_appauth
+import flutter_secure_storage_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
