2d7fed8ddc1b4f63067e1fcb47109ad328918b2c
Muhammed Rijas <155318185+rijas0@users.noreply.github.com>
2026-05-13 19:51:19 +0530

    circle join impl

--- lib/core/api/app_api.dart
+++ lib/core/api/app_api.dart
@@ -7,6 +7,6 @@ const String apiBaseUrl = 'https://apis-prelive.quran.foundation';
 final String authorizationEndpoint = '$authBaseUrl/oauth/authorize';
 final String tokenEndpoint = '$authBaseUrl/oauth2/token'; 
 final String circleCreationEndpoint = 'https://qyyvgxrypolktxowctwu.supabase.co/functions/v1/createCircle';
-final String joinCircleEndpoint = 'https://qyyvgxrypolktxowctwu.supabase.co/functions/v1/joinCircle';
+final String joinCircleEndpoint = 'https://qyyvgxrypolktxowctwu.supabase.co/functions/v1/joinRoom';
 final String homeDetailsEndpoint = '';
 final String circleDetailsEndpoint = '';
